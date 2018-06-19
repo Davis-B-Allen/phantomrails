8f8c63640dac425ef6465e17a8c96b28ceb1a850
Davis Allen <davis.b.allen@gmail.com>
2018-06-19 16:35:53 -0400

    Add basic phantomjs call

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,12 @@ class ApplicationController < ActionController::Base
   def hello
     render html: "hello, phantomjs!"
   end
+
+  def cardgen
+    output_folder = Rails.root.join("tmp", "phantom/").to_s
+    phantomjs_script = Rails.root.join("lib", "assets", "phantomtest.js").to_s
+    system "phantomjs \"#{phantomjs_script}\" \"#{output_folder}\""
+    puts "************** DONE ***************"
+    render html: "CARDGEN"
+  end
 end
